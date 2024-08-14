f4611dc4c8fe4f11221f9b4db208231619844b05
Milad Shahabifard <milad.shahabi.20@gmail.com>
2024-08-14 10:40:42 +0200

    add loading component for after pressing buttons

--- components/Button.jsx
+++ components/Button.jsx
@@ -2,18 +2,35 @@ import { Pressable, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { theme } from "../constants/theme";
 import { hp } from "../helpers/common";
+import Loading from "./Loading";
 
 const Button = ({
   buttonStyle,
   textStyle,
-  title = '',
+  title = "",
   onPress = () => {},
   loading = false,
   hasShadow = true,
 }) => {
-    const shadowStyle = {}
+  const shadowStyle = {
+    shadowColor: theme.colors.black,
+    shadowOffset: { width: 0, height: 10 },
+    shadowOpacity: 0.2,
+    shadowRadius: 8,
+    elevation: 4,
+  };
+  if (loading) {
+    return (
+      <View style={[styles.button, buttonStyle, { backgroundColor: "white" }]}>
+        <Loading />
+      </View>
+    );
+  }
   return (
-    <Pressable onPress={onPress} style={[styles.button, buttonStyle, hasShadow && shadowStyle]}> 
+    <Pressable
+      onPress={onPress}
+      style={[styles.button, buttonStyle, hasShadow && shadowStyle]}
+    >
       <Text style={[styles.text, textStyle]}>{title}</Text>
     </Pressable>
   );
@@ -22,19 +39,18 @@ const Button = ({
 export default Button;
 
 const styles = StyleSheet.create({
-    button: {
-        backgroundColor: theme.colors.secondary,
-        height: hp(6.6),
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderCurve: 'continous',
-        borderRadius: theme.radius.xl,
-    },
-    text: {
-        color: theme.colors.dark,
-        fontSize: hp(2.5),
-        fontWeight: theme.fonts.medium,
-        //textTransform: 'uppercase',
-    },
-  
+  button: {
+    backgroundColor: theme.colors.secondary,
+    height: hp(6.6),
+    justifyContent: "center",
+    alignItems: "center",
+    borderCurve: "continous",
+    borderRadius: theme.radius.xl,
+  },
+  text: {
+    color: theme.colors.dark,
+    fontSize: hp(2.5),
+    fontWeight: theme.fonts.medium,
+    //textTransform: 'uppercase',
+  },
 });
